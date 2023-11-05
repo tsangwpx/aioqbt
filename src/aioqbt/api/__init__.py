2524abe94c374f7d1d38d83834845548522fb608
Aaron <1143023+tsangwpx@users.noreply.github.com>
2023-11-06 00:39:43 +0800

    Re-export symbols in aioqbt.api

--- src/aioqbt/api/__init__.py
+++ src/aioqbt/api/__init__.py
@@ -4,6 +4,8 @@ API methods by group.
 See also :APIWiki:`the WebUI API reference <>` on qBittorrent wiki.
 """
 
+# re-export API groups, constants, and types here
+
 from aioqbt.api.app import AppAPI
 from aioqbt.api.auth import AuthAPI
 from aioqbt.api.log import LogAPI
@@ -11,6 +13,44 @@ from aioqbt.api.sync import SyncAPI
 from aioqbt.api.torrents import AddFormBuilder, TorrentsAPI
 from aioqbt.api.transfer import TransferAPI
 
+# isort: split
+from aioqbt.api.types import (
+    ConnectionStatus,
+    ContentLayout,
+    FilePriority,
+    InactiveSeedingTimeLimits,
+    InfoFilter,
+    PieceState,
+    RatioLimits,
+    SeedingTimeLimits,
+    StopCondition,
+    TorrentState,
+    TrackerStatus,
+)
+
+# isort: split
+from aioqbt.api.types import (
+    BuildInfo,
+    Category,
+    FileEntry,
+    LogMessage,
+    LogPeer,
+    NetworkInterface,
+    Preferences,
+    SyncCategory,
+    SyncMainData,
+    SyncPeer,
+    SyncServerState,
+    SyncTorrentInfo,
+    SyncTorrentPeers,
+    TorrentInfo,
+    TorrentProperties,
+    Tracker,
+    TransferInfo,
+    WebSeed,
+)
+
+# wildcard imports are discouraged
 __all__ = (
     "AddFormBuilder",
     "AppAPI",
